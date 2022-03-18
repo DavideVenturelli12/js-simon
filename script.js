3dcca197778bbac255b7d8834a941ac595f44962
Davide Venturelli <dventurelli90@gmail.com>
2022-03-18 16:29:51 +0100

    Creazione numeri casuali + timer

--- script.js
+++ script.js
@@ -12,5 +12,46 @@ precedentemente, tramite il prompt().
 numeri da indovinare sono stati individuati.
 */
 
+const container = document.getElementById('container');
+
+const arrayNumber = 5;
+
+ArrayGenerator(arrayNumber);
+
+function ArrayGenerator(arrayNumber) {
+    let array = [];
+    for (let i = 0; i < 5; i++) {
+        array.push(Math.floor(Math.random() * 50 + 1));
+        const cella = creaCella();
+        cella.innerText = array[i];
+        container.appendChild(cella);
+    }
+    console.log(array);
+}
+
+function creaCella() {
+    const cella = document.createElement('div');
+    cella.classList.add('cella');
+    return cella;
+}
 
 
+let max = 30;
+
+const timer = document.getElementById('timer');
+timer.innerText = max;
+
+const number = setInterval(myFunctionInterval, 1000); // verrà eseguito ogni secondo (1000ms)
+
+function myFunctionInterval() {
+
+    max--;
+    timer.innerText = max;
+    //console.log(max);
+    if (max <= 0) {
+        // cancello il setInterval
+        clearInterval(number);
+        timer.innerText = 'Tempo scaduto';
+    }
+
+}
